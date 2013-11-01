94e1aabff8a31f6f1c50daa0d58fbf1249405f92
Kev-L <kevlambe@gmail.com>
2013-11-01 15:52:34 +0000

    Added KeyListener to label.

--- MiniProject.MessingWithFrames/src/frame/GUI.java
+++ MiniProject.MessingWithFrames/src/frame/GUI.java
@@ -1,8 +1,58 @@
 package frame;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.*;
+
 public class GUI {
-	public static void main(String[] args){
-		System.out.println("Hello GIT");
-		System.out.println("Testing");
+	JLabel label1;
+	
+	
+	public GUI(){
+		JFrame frame = new JFrame();
+		label1 = new JLabel();
+		label1.setFocusable(true);
+		label1.setPreferredSize(new Dimension(480,640));
+		label1.setVerticalAlignment(SwingConstants.TOP);
+		frame.add(label1);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
+	public void setLabel(){
+		
+		label1.addKeyListener(new KeyListener(){
+			String text="";
+			Random n = new Random();
+			int x;
+			@Override
+			public void keyPressed(KeyEvent e) {
+				x = n.nextInt()%10+1;
+				if(x<7&&x>3)
+					text = text +"<br>";
+				for(int i = 0;i<x;i++){
+					text = text + " blah";
+				}
+				label1.setText("<html>"+text+"</html>");
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 }
--- MiniProject.MessingWithFrames/src/main/Main.java
+++ MiniProject.MessingWithFrames/src/main/Main.java
@@ -3,7 +3,9 @@ package main;
 import frame.GUI;
 
 public class Main {
+	
 	public static void main(String[] args){
 		GUI g = new GUI();
+		g.setLabel();
 	}
 }
